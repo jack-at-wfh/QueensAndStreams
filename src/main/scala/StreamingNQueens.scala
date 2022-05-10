a61035f28746af80f851d58fbb036073594a9b04
Jack Wells <jack939@tpg.com.au>
2022-05-10 21:19:21 +1000

    Updated comments on nQueensSolution

--- src/main/scala/StreamingNQueens.scala
+++ src/main/scala/StreamingNQueens.scala
@@ -169,7 +169,7 @@ object StreamingNQueens extends zio.ZIOAppDefault {
       0 -> 1, 1 -> 1, 2 -> 0, 3 -> 0, 4 -> 2, 5 -> 0, 6 -> 4, 7 -> 40, 8 -> 92,
       9 -> 352,10 -> 724, 11 -> 2680, 12 -> 14200, 13 -> 73712,14 -> 365596,
       15 -> 2279184
-    )
+    ) //NQueens is a well-known problem see https://oeis.org/A000170
 
   implicit val chessBoard: Int = 10
 
